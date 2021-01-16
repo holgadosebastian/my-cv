d16c6bd992ba8fca92a73f7cc9e16df0ec7ae6f9
holgadosebastian <holgado.sebastian@gmail.com>
2021-01-16 19:48:59 +0100

    Update tailwind.config.js purge safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-  purge: [
-    'src/**/*.js',
-    'src/**/*.jsx',
-    'src/**/*.ts',
-    'src/**/*.tsx',
-    'public/**/*.html',
-  ],
+  purge: {
+    content: [
+      'src/**/*.js',
+      'src/**/*.jsx',
+      'src/**/*.ts',
+      'src/**/*.tsx',
+      'public/**/*.html',
+    ],
+    options: {
+      safelist: ['md:w-2/12', 'md:w-3/12', 'md:w-6/12', 'md:w-7/12', 'md:w-9/12', 'print:w-2/12', 'print:w-3/12', 'print:w-6/12', 'print:w-7/12', 'print:w-9/12'],
+    },
+  },
   theme: {
     fontFamily: {
       'display': 'Source Sans Pro',
